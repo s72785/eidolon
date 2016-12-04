02851471a19d62d516433266d1e35f48d8ce0894
Wolf <s72785@users.noreply.github.com>
2016-12-04 11:59:50 +0100

    librejs #28

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL v3.0
 var username = $('#username');
 var hint = $('#usernamehint');
 function fixUsername() {
@@ -16,3 +17,4 @@ function fixUsername() {
 username.bind('change', fixUsername);
 username.bind('input', fixUsername);
 username.bind('keyup', fixUsername);
+// @license-end
